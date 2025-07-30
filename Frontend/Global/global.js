bf80f87df4b0f3c995f96ab984cc8ee7fc1c44da
KelvinANketia <kelvinkwabenanketia@gmail.com>
2025-07-30 22:34:07 +0000

    Menu MQ

--- Frontend/Global/global.js
+++ Frontend/Global/global.js
@@ -1,5 +1,9 @@
 // Global navigation routing for .menu-btn elements
 document.addEventListener('DOMContentLoaded', function () {
+    const toggleMenuBtn = document.querySelector('.tg-menu');
+    const sideMenu = document.querySelector('.side-menu');
+    const sideMenuOptions = sideMenu.querySelectorAll('a');
+
     const routes = {
         'Home': '/home',
         'News & Blog': '/news-blog',
@@ -28,4 +32,32 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         });
     });
+
+    document.addEventListener('click', (event) => {
+        const isMenuBtnClicked = toggleMenuBtn.contains(event.target);
+        const isSideMenuOpen = !sideMenu.classList.contains('closed');
+        const isSideMenuClosed = sideMenu.classList.contains('closed');
+        const isClickOutsideSideMenu = !sideMenu.contains(event.target);
+        const isClickOutsideMenuOptions = !Array.from(sideMenuOptions).some(opt => opt.contains(event.target));
+
+        if (isMenuBtnClicked) {
+            sideMenu.classList.toggle('closed');
+            if (sideMenu.classList.contains('closed')) {
+                // Reset everything when menu is closed
+                document.querySelector('nav').style.position = '';
+                toggleMenuBtn.style.backgroundColor = '';
+            } else {
+                document.querySelector('nav').style.position = 'relative';
+                toggleMenuBtn.style.backgroundColor = 'hsl(220, 13%, 91%)';
+            }
+        } else if (shouldCloseSideMenu.call(this, isSideMenuOpen, isClickOutsideSideMenu, isClickOutsideMenuOptions)) {
+            sideMenu.classList.add('closed');
+            document.querySelector('nav').style.position = 'sticky';
+            toggleMenuBtn.style.backgroundColor = '';
+        }
+    });
+
+    function shouldCloseSideMenu(isSideMenuOpen, isClickOutsideSideMenu, isClickOutsideMenuOptions) {
+        return isSideMenuOpen && isClickOutsideSideMenu && isClickOutsideMenuOptions;
+    }
 });
